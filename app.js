60313034e253e607c91b8f8daee68380a3f5b194
femiliapm <femilia.putri@ui.ac.id>
2023-07-21 10:03:03 +0700

    add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 // routing
 const bootcampsRouter = require("./src/routes/bootcamp-router");
@@ -18,6 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "*", // or specific url
+  })
+);
 
 // routing
 app.use("/api/bootcamp", bootcampsRouter);
