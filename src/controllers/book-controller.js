481cce980fab326ecf0aa5e6347f68cc3eb634da
femiliapm <femilia.putri@ui.ac.id>
2023-07-26 08:41:35 +0700

    validasi request

--- src/controllers/book-controller.js
+++ src/controllers/book-controller.js
@@ -3,14 +3,26 @@ const { responseError, responseData } = require("../utils/response-handler");
 
 exports.createBook = async (req, res) => {
   try {
-    const requestBody = { ...req.body };
+    const { title, author, publisher } = { ...req.body };
+    if (
+      title === undefined ||
+      author === undefined ||
+      publisher === undefined
+    ) {
+      return responseError(res, 400, "Bad request!");
+    }
+    const requestBody = {
+      title,
+      author,
+      publisher,
+    };
     const result = await bookModel.addBook(requestBody);
 
     if (!result.affectedRows) {
       return responseError(res, 500, "Internal server error!", error);
     }
 
-    let data = await bookModel.getBooksById(res, result.insertId);
+    let data = await bookModel.getBooksById(result.insertId);
     return responseData(res, 201, data);
   } catch (error) {
     return responseError(res, 500, "Internal server error!", error);
@@ -29,6 +41,9 @@ exports.getBooks = async (req, res) => {
 exports.getBookById = async (req, res) => {
   try {
     const id = req.params.id;
+    if (id === undefined) {
+      return responseError(res, 400, "Bad request!");
+    }
     let data = bookModel.getBooksById(id);
     return responseData(res, 200, data);
   } catch (error) {
